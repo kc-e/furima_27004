aa0b75923b9676a6f71b2743d7579042085eabd3
kc-e <66234115+kc-e@users.noreply.github.com>
2020-07-09 16:42:20 +0900

    updated

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,12 +4,12 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.order('created_at DESC')
+    @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-    if @item.valid?
+    if @item.save
       redirect_to root_path
     else
       render :new
