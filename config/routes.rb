6ec58510d04dd1d190cb18bc84da0ca80e0da4b6
kc-e <66234115+kc-e@users.noreply.github.com>
2020-07-11 14:19:46 +0900

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'items#index'
-  resources :items, only: %i[index new create show]
+  resources :items, only: %i[index new create show edit update]
   # resources
   # get 'users/sign_in', to: 'devise/sessions#create'
   # resources :devise, only: [:index, :new, :create]
